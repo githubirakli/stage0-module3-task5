be2315cb50f50db5d7a236a612d7e8d500a848aa
githubirakli <118377638+githubirakli@users.noreply.github.com>
2023-09-24 08:56:38 +0400

    Update TimeConvertor.java

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public static void main(String[] args) {
+        float minutes=5.5f;
+        float seconds=minutes*60;
+        System.out.println(minutes+" minutes is equal to "+seconds+" seconds");
+        
     }
+
 }
